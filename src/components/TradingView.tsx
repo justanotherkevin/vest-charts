922c2bbc31fbe583a9556ecf5cddb7328f296e9d
justanotherkevin <kevin.hu@quizlet.com>
2024-12-14 19:13:33 -0500

    Add tabs to main container

--- src/components/TradingView.tsx
+++ src/components/TradingView.tsx
@@ -1,25 +1,51 @@
+import { useState } from "react";
+import styled from "styled-components";
 import OrderForm from "./OrderForm";
 import TradingChart from "./TradingChart";
 import TradingHeader from "./TradingHeader";
+import * as S from "./styles/utility";
 
-const TradingView = () => (
-  <div className="trading-view">
-    <TradingHeader
-      symbol="BTC/BITCOIN"
-      price={31119.01}
-      change24h="+22.3 USDC (+7.5%)"
-      funding="0.00012%"
-      longInterest="8.871 BTC"
-      shortInterest="8.871 BTC"
-    />
-    <div className="trading-content">
-      <div className="chart-section borderrr">
-        <TradingChart />
-      </div>
-      <div className="order-section">
-        <OrderForm side="LONG" />
-      </div>
+const GridContainer = styled.div`
+  display: grid;
+  column-gap: 16px;
+  grid-template-columns: minmax(640px, auto) 360px;
+
+  justify-items: stretch;
+`;
+const TradingView = () => {
+  const [side, setSide] = useState<"PRICE" | "FUNDING">("PRICE");
+  return (
+    <div className="trading-view">
+      <TradingHeader
+        symbol="BTC/BITCOIN"
+        price={31119.01}
+        change24h="+22.3 USDC (+7.5%)"
+        funding="0.00012%"
+        longInterest="8.871 BTC"
+        shortInterest="8.871 BTC"
+      />
+
+      <S.TabButtons>
+        <S.TabButton active={side === "PRICE"} onClick={() => setSide("PRICE")}>
+          PRICE
+        </S.TabButton>
+        <S.TabButton
+          active={side === "FUNDING"}
+          onClick={() => setSide("FUNDING")}
+        >
+          FUNDING
+        </S.TabButton>
+      </S.TabButtons>
+
+      <GridContainer>
+        <div className="chart-section borderrr">
+          <TradingChart />
+        </div>
+        <div className="order-section borderrr">
+          <OrderForm />
+        </div>
+      </GridContainer>
     </div>
-  </div>
-);
+  );
+};
 export default TradingView;
